24138a728f24b5b084c349f684551df02f491d0b
Aristofanis Chionis <aristofanis.chionis@cern.ch>
2020-02-26 16:50:05 +0100

    fixed and improvements in the Item component

--- app/containers/Editor/MySlide.js
+++ app/containers/Editor/MySlide.js
@@ -3,27 +3,30 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 // import MyText from './components/text';
 // import MyImage from './components/image';
+import MyItem from './components/Item';
 import StandardSlide from '../../theming/StandardSlide';
 import { getTheme } from '../redux-store/PresentationReducer/selectors';
-
-function MySlide({ theme }) {
+import { getItems } from '../redux-store/DeckReducer/selectors';
+function MySlide({ theme, itemsArray }) {
   const StandardSlideTemplate = StandardSlide(theme);
   return (
     <StandardSlideTemplate>
-      {/* <MyText />
-      <MyImage /> */}
-      <h1>Hello my Slide</h1>
+      {itemsArray.map(itm => (
+        <MyItem key={itm.ID} itemObj={itm} />
+      ))}
     </StandardSlideTemplate>
   );
 }
 
 MySlide.propTypes = {
   theme: PropTypes.string.isRequired,
+  itemsArray: PropTypes.array,
 };
 
 export default connect(
   state => ({
     theme: getTheme(state),
+    itemsArray: getItems(state),
   }),
   null,
 )(MySlide);
--- app/containers/Editor/components/Item/index.js
+++ app/containers/Editor/components/Item/index.js
@@ -11,14 +11,158 @@
 // its parent component is an array of them
 // i have only one array of elements in the redux but has a type  text/image
 // depending on that the obj is different and will be rendered differently
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import KeyboardEventHandler from 'react-keyboard-event-handler';
+import axios from 'axios';
+import { Rnd } from 'react-rnd';
+import {
+  removeItem,
+  changeItemPosition,
+  changeItemSize,
+  setEditMode,
+} from '../../../redux-store/DeckReducer/actions';
+import { getAssetsPath } from '../../../redux-store/PresentationReducer/selectors';
+import './index.css';
 
-function Item({ itemObj }) {
-  const ref = useRef(null);
-  const { position, size, type } = itemObj;
+const style = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  position: 'absolute',
+};
+
+function Item({
+  itemObj,
+  onRemoveItem,
+  onChangePosition,
+  onChangeSize,
+  assetsPath,
+  onSetEditMode,
+}) {
+  const itemRef = useRef(null);
+  const { Position, Size, type } = itemObj;
+  const [curSize, setCurSize] = useState(Size);
+  const [curPosition, setCurPosition] = useState(Position);
+  const [focused, setFocused] = useState(false);
+  console.log('itemObj', itemObj);
   // this is something that doesn't need to be stored in store
   // will change in with clicks
-  const [focused, setFocused] = useState(false);
+  const handleDragStop = (e, pos) => {
+    e.preventDefault();
+    setCurPosition(pos);
+    onChangePosition(itemObj.ID, pos);
+  };
+
+  const handleResizeStop = (e, siz) => {
+    e.preventDefault();
+    setCurSize(siz);
+    onChangeSize(itemObj.ID, siz);
+  };
+  // const delImageReq = () => {
+  //   const url = `${assetsPath}/image/${imageObj.src}`;
+  //   return axios.delete(url);
+  // };
+
+  const deleter = e => {
+    e.preventDefault();
+    console.log('deleter called');
+    // send a delete in Redux
+    // onRemoveItem(imageObj.src);
+    // send a delete in Server if it is an Image
+    // delImageReq();
+  };
+
+  const onDoubleClick = evt => {
+    evt.preventDefault();
+    if (itemRef.current.contains(evt.target)) {
+      // inside click
+      // then edit
+      // console.log('double click');
+      onChangePosition(itemObj.ID, curPosition);
+      onChangeSize(itemObj.ID, curSize);
+      onSetEditMode(itemObj.ID, true);
+    }
+  };
+
+  const handleClick = e => {
+    e.preventDefault();
+    if (itemRef.current.contains(e.target)) {
+      // inside click
+      itemRef.current.focus();
+      setFocused(true);
+      // then move
+      return;
+    }
+    // outside click
+    // console.log('outside clicki');
+    itemRef.current.blur();
+    setFocused(false);
+    // onAddData(currentText, text);
+    onSetEditMode(itemObj.ID, false);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  });
+
+  // deleter only called when the element is focused
+
+  return (
+    <div ref={itemRef}>
+      <Rnd
+        className="item-style"
+        style={style}
+        size={{ width: curSize.width, height: curSize.height }}
+        position={{ x: curPosition.x, y: curPosition.y }}
+        onDragStop={(e, d) => handleDragStop(e, { x: d.x, y: d.y })}
+        onResizeStop={(e, direction, ref, delta, position) => {
+          handleResizeStop(e, {
+            width: ref.style.width,
+            height: ref.style.height,
+          });
+        }}
+        minWidth={500}
+        minHeight={70}
+        onDoubleClick={onDoubleClick}
+      >
+        <KeyboardEventHandler
+          handleKeys={['backspace', 'del']}
+          onKeyEvent={(key, e) => focused && deleter(e)}
+        />
+        <h1>Hello my item</h1>
+      </Rnd>
+    </div>
+  );
 }
+
+Item.propTypes = {
+  itemObj: PropTypes.object,
+  assetsPath: PropTypes.string,
+  onRemoveItem: PropTypes.func,
+  onChangePosition: PropTypes.func,
+  onChangeSize: PropTypes.func,
+  onSetEditMode: PropTypes.func,
+};
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onChangePosition: (id, position) =>
+      dispatch(changeItemPosition(id, position)),
+    onChangeSize: (id, position) => dispatch(changeItemSize(id, position)),
+    onRemoveItem: id => dispatch(removeItem(id)),
+    onSetEditMode: (id, edit) => dispatch(setEditMode(id, edit)),
+  };
+}
+
+export default connect(
+  state => ({
+    assetsPath: getAssetsPath(state),
+  }),
+  mapDispatchToProps,
+)(Item);
