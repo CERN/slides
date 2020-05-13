3b0627c99f0d5e07b32498ba9082644e193c185a
aristofanischionis <aristofanischionis@gmail.com>
2020-05-13 13:54:20 +0300

    trying to fix image uploading speed

--- packages/slides-frontend/src/containers/Editor/components/image/index.js
+++ packages/slides-frontend/src/containers/Editor/components/image/index.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {
   getAssetsPath,
   getTitle,
 } from '../../../redux-store/PresentationReducer/selectors';
-
+import { Loader } from 'semantic-ui-react';
 import { getItems } from '../../../redux-store/DeckReducer/selectors';
 import './index.css';
 
@@ -13,12 +13,16 @@ const Image = ({ ID, itemsArray, assetsPath, username, title }) => {
   const item = itemsArray.find(itm => itm.ID === ID);
   // this base will be the server's address base for every image , localhost:3000/static/username/title/hash_imagename
   // src only has hash_name, i have to add username and title infront
-  const myPath = `${assetsPath}/static/${username}/${title}/assets/${item.Src}`;
   return (
     <div>
-      <div className="img-style">
-        <img src={myPath} alt="" />
-      </div>
+      {item ?
+        (
+          <div className="img-style">
+            <img src={`${assetsPath}/static/${username}/${title}/assets/${item.Src}`} alt="" />
+          </div>
+        ) :
+        <Loader active/>
+      }
     </div>
   );
 };
