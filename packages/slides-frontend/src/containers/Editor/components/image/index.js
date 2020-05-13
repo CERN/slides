7a278a67bc29885a693da6e02bda0ce39761d700
aristofanischionis <aristofanischionis@gmail.com>
2020-05-13 12:58:59 +0300

    small changes in image

--- packages/slides-frontend/src/containers/Editor/components/image/index.js
+++ packages/slides-frontend/src/containers/Editor/components/image/index.js
@@ -9,20 +9,16 @@ import {
 import { getItems } from '../../../redux-store/DeckReducer/selectors';
 import './index.css';
 
-// FIX positioning of image
 const Image = ({ ID, itemsArray, assetsPath, username, title }) => {
-  const [ loader, setLoader ] = useState(true);
   const item = itemsArray.find(itm => itm.ID === ID);
   // this base will be the server's address base for every image , localhost:3000/static/username/title/hash_imagename
   // src only has hash_name, i have to add username and title infront
   const myPath = `${assetsPath}/static/${username}/${title}/assets/${item.Src}`;
-  useEffect(() => {
-    setTimeout(() => setLoader(false), 1000) // ?? add loader
-  }, []);
   return (
-    !loader &&
-    <div className="img-style">
-      <img src={myPath} alt="" />
+    <div>
+      <div className="img-style">
+        <img src={myPath} alt="" />
+      </div>
     </div>
   );
 };
