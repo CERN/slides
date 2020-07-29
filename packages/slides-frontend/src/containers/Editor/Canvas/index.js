38940cf3f0b3cb962632bfd6c7e15dbec711e131
Aristofanis Chionis <aristofanis.chionis@cern.ch>
2020-07-29 16:24:55 +0200

    Start using reveal.js instead of Spectacle
    Add the reveal.js library and make it possible to show the leftmost bars
    and the reveal.js presentation in the rest of the space. Worked!

--- packages/slides-frontend/src/containers/Editor/Canvas/index.js
+++ packages/slides-frontend/src/containers/Editor/Canvas/index.js
@@ -1,9 +1,8 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {Helmet} from 'react-helmet';
 import {connect} from 'react-redux';
 import {FlexBox, Box, FullScreen, Progress, Deck, Slide} from 'spectacle';
-// import createTheme from 'spectacle/lib/themes/default/index';
 import history from '../../../utils/history';
 import {deletePresentationFolder} from '../../../utils/requests';
 
@@ -19,73 +18,31 @@ import MoveResize from '../components/resize_move';
 
 import MySlide from '../MySlide';
 import './index.css';
-// import getterTheme from '../../../theming/theme';
+import Reveal from 'reveal.js';
+import 'reveal.js/dist/reveal.css';
+// import 'reveal.js/dist/theme/beige.css';
+import '../../../theming/cern3.css';
 
 function Canvas({title, theme, DeckOfSlides, assetsPath, backgroundColor, username, token, currentSlide}) {
-  const deck = useRef();
-  // const myTheme = getterTheme(theme);
-  // change fontconfig from here
-  // const newTheme = {
-  //   ...themeObj,
-  //   themeConfig: {
-  //     ...themeObj.themeConfig,
-  //     secondary: backgroundColor,
-  //   },
-  // };
-
-  // const myTheme = createTheme(newTheme.themeConfig, newTheme.fontConfig);
-  const myTheme = {
-    colors: {
-      primary: '#f00',
-      secondary: '#00f'
-    },
-    fontSizes: {
-      header: '64px',
-      paragraph: '28px'
-    }
-  }
-  // // now make the check if it is cern 3,4,5 then add intro and end slide
-  // // use this hook to be able to move to next previous slide in adding removing slides
-  // useEffect(() => {
-  //   window.slideCount = deck.current.props.children.length;
-  // });
- // somehow try to sync in the new version what is seen with the URL
- // the location change is not triggered always but only when adding a new slide
- // thus leading to not changing the currentSlide when user uses the arrow keys
-  const slidesArray = () =>
-    DeckOfSlides.map(item => (
-      <Slide>
-        <MySlide key={item.ID}/>
-      </Slide>
-    ))
-
-  // catch reload event
   useEffect(() => {
-    window.onbeforeunload = e => {
-      e.preventDefault();
-      deletePresentationFolder(assetsPath, username, title, token).then(res => {
-        // and navigate to home page
-        if (res.status === 200) {
-          console.log('Successful deletion in Server');
-          history.push(`/`);
-        }
-      });
-      return 'Are you sure you want to reload?';
-    };
-  });
+    Reveal.initialize({
+      showSlideNumber: 'all',
+      hash: true,
+      respondToHashChanges: true,
+      history: true,
+      keyboard: true,
+      embedded: true,
+      // Transition style
+      transition: 'slide', // none/fade/slide/convex/concave/zoom
+      // Transition speed
+      transitionSpeed: 'default', // default/fast/slow
+      // Transition style for full page slide backgrounds
+      backgroundTransition: 'fade', // none/fade/slide/convex/concave/zoom
+    }).then(() => {
+      console.log("Reveal.js is initialized");
+    });
 
-  const template = () => (
-      <FlexBox
-      justifyContent="space-between"
-      position="absolute"
-      bottom={0}
-      width={1}
-    >
-      <Box margin={0, 2}>
-      {currentSlide + 1} / {DeckOfSlides.length}
-    </Box>
-    </FlexBox>
-  );
+  });
 
   return (
     <div>
@@ -93,24 +50,114 @@ function Canvas({title, theme, DeckOfSlides, assetsPath, backgroundColor, userna
         <title>Edit: {title}</title>
       </Helmet>
       <div className="deck">
-        <Deck
-          ref={deck}
-          // transition={['zoom', 'slide']}
-          // transitionDuration={500}
-          theme={myTheme}
-          // progress="number"
-          animationsWhenGoingBack
-          keyboardControls="arrows"
-          // template={template}
-          template={template}
-          // showFullscreenControl={false}
-        >
-        {slidesArray()}
-        </Deck>
+          <div className="reveal">
+            <div className="slides">
+              <section><h2>First Horizontal Slide 🐟</h2></section>
+              <section>
+                <section>
+                  Second slide
+                </section>
+                <section>
+                  The only vertical slide!
+                </section>
+              </section>
+              <section>Third slide!!!!!!!!!!!!!!!</section>
+              <section>Fourth slide!!!!!!!!!!!!!!!</section>
+              <section>Fifth slide!!!!!!!!!!!!!!!</section>
+            </div>
+          </div>
       </div>
     </div>
-  );
+  )
 }
+
+// function Canvas({title, theme, DeckOfSlides, assetsPath, backgroundColor, username, token, currentSlide}) {
+//   const deck = useRef();
+//   // const myTheme = getterTheme(theme);
+//   // change fontconfig from here
+//   // const newTheme = {
+//   //   ...themeObj,
+//   //   themeConfig: {
+//   //     ...themeObj.themeConfig,
+//   //     secondary: backgroundColor,
+//   //   },
+//   // };
+
+//   // const myTheme = createTheme(newTheme.themeConfig, newTheme.fontConfig);
+//   const myTheme = {
+//     colors: {
+//       primary: '#f00',
+//       secondary: '#00f'
+//     },
+//     fontSizes: {
+//       header: '64px',
+//       paragraph: '28px'
+//     }
+//   }
+//   // // now make the check if it is cern 3,4,5 then add intro and end slide
+//   // // use this hook to be able to move to next previous slide in adding removing slides
+//   // useEffect(() => {
+//   //   window.slideCount = deck.current.props.children.length;
+//   // });
+//  // somehow try to sync in the new version what is seen with the URL
+//  // the location change is not triggered always but only when adding a new slide
+//  // thus leading to not changing the currentSlide when user uses the arrow keys
+//   const slidesArray = () =>
+//     DeckOfSlides.map(item => (
+//       <Slide>
+//         <MySlide key={item.ID}/>
+//       </Slide>
+//     ))
+
+//   // catch reload event
+//   useEffect(() => {
+//     window.onbeforeunload = e => {
+//       e.preventDefault();
+//       deletePresentationFolder(assetsPath, username, title, token).then(res => {
+//         // and navigate to home page
+//         if (res.status === 200) {
+//           console.log('Successful deletion in Server');
+//           history.push(`/`);
+//         }
+//       });
+//       return 'Are you sure you want to reload?';
+//     };
+//   });
+
+//   const template = () => (
+//       <FlexBox
+//       justifyContent="space-between"
+//       position="absolute"
+//       bottom={0}
+//       width={1}
+//     >
+//       <Box margin={0, 2}>
+//       {currentSlide + 1} / {DeckOfSlides.length}
+//     </Box>
+//     </FlexBox>
+//   );
+
+//   return (
+//     <div>
+//       <Helmet>
+//         <title>Edit: {title}</title>
+//       </Helmet>
+//       <div className="deck">
+//         <Deck
+//           ref={deck}
+//           // transition={['zoom', 'slide']}
+//           // transitionDuration={500}
+//           theme={myTheme}
+//           animationsWhenGoingBack
+//           keyboardControls="arrows"
+//           template={template}
+//         >
+//         {slidesArray()}
+//         </Deck>
+//       </div>
+//     </div>
+//   );
+// }
 
 Canvas.propTypes = {
   title: PropTypes.string,
